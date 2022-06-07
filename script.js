2e5d9a6c82e5384b20e3a056c56276e9671b0cfd
TZane25 <83322334+TZane25@users.noreply.github.com>
2022-06-07 23:59:57 +0800

    Update script.js
    testing js functionality

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+$(document).ready(function () {
 
+
+
+  var scroll_pos = 0;
+
+  $(document).scroll(function () {
+      scroll_pos = $(this).scrollTop();
+
+      if (scroll_pos <= 1700) { 
+
+        $("#navbarNav").css('background-color', 'transparent'); // change the bottom color // elgengroid
+         
+
+      }
+      else if (scroll_pos >= 1700 || scroll_pos <= 3400){
+        $("#navbarNav").css('background-color', 'white');
+        $('#navbarNav').css('transition','transform 1s ease');
+
+        $(".nav-item").css('color', 'black' , 'transition','transform 1s ease')
+
+      }
+      else 
+      {
+        $("#navbarNav").css('background-color', 'yellow'); // black
+
+
+      }
+  });
+});
+
+function topFunction() {
+
+  $('html, body').animate({ scrollTop: 0 }, 'slow'); // top function
+}
